c13f36727cad7543c98e2c8a7df8b9f6291b4bf6
James Croft <jamz_c@hotmail.co.uk>
2017-03-04 16:55:37 +0000

    UWP - Custom control development push.

---
+++ ControlDevelopment/ControlDevelopment.TestApp/TemplatedControls/MyTemplatedControl/MyTemplatedControl.cs
@@ -0,0 +1,78 @@
+﻿namespace ControlDevelopment.Controls
+{
+    using System;
+
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media;
+
+    public class MyTemplatedControl : Control
+    {
+        public static readonly DependencyProperty SomeTextProperty = DependencyProperty.Register(
+            nameof(SomeText),
+            typeof(string),
+            typeof(MyTemplatedControl),
+            new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(
+            nameof(ImageSource),
+            typeof(ImageSource),
+            typeof(MyTemplatedControl),
+            new PropertyMetadata(null, OnImageSourceChanged));
+
+        private static void OnImageSourceChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var control = dependencyObject as MyTemplatedControl;
+            if (control != null)
+            {
+                control.UpdateUI((Windows.UI.Xaml.Media.ImageSource)dependencyPropertyChangedEventArgs.NewValue);
+            }
+        }
+
+        private void UpdateUI(ImageSource newValue)
+        {
+            
+        }
+
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return (ImageSource)GetValue(ImageSourceProperty);
+            }
+            set
+            {
+                SetValue(ImageSourceProperty, value);
+            }
+        }
+
+        public MyTemplatedControl()
+        {
+            this.DefaultStyleKey = typeof(MyTemplatedControl);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var image = GetTemplateChild("Image") as Image;
+            if (image != null)
+            {
+                image.Stretch = Stretch.UniformToFill;
+            }
+        }
+
+        public string SomeText
+        {
+            get
+            {
+                return (string)this.GetValue(SomeTextProperty);
+            }
+
+            set
+            {
+                this.SetValue(SomeTextProperty, value);
+            }
+        }
+    }
+}
---
+++ ControlDevelopment/ControlDevelopment.TestApp/UserControls/MyUserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace ControlDevelopment.Controls
+{
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class MyUserControl : UserControl
+    {
+        public static readonly DependencyProperty SomeTextProperty = DependencyProperty.Register(
+            nameof(SomeText),
+            typeof(string),
+            typeof(MyUserControl),
+            new PropertyMetadata(string.Empty));
+
+        public MyUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public string SomeText
+        {
+            get
+            {
+                return (string)this.GetValue(SomeTextProperty);
+            }
+
+            set
+            {
+                this.SetValue(SomeTextProperty, value);
+            }
+        }
+    }
+}
